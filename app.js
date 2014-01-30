2fdffbb94cc9c4e0ace2d2c592749dadaf395eed
George Bonner <georgebonnr@gmail.com>
2014-01-30 01:52:52 -0800

    heroku

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
 var express = require('express');
 var app = express();
 var fs = require('fs');
+var logfmt = require("logfmt");
+var port = Number(process.env.PORT || 5000);
 
 app.configure(function(){
   app.use(express.static(__dirname + '/public'));
+  app.use(logfmt.requestLogger());
 });
 
 app.get('/', function(req, res){
-  console.log('hi');
   fs.readFile('index.html', function (err, data) {
     if (err) throw err;
     res.setHeader('Content-Type', 'text/html');
@@ -15,4 +17,6 @@ app.get('/', function(req, res){
   });
 });
 
-app.listen(3000);
+app.listen(port, function(){
+  console.log("Listening on " + port);
+});
